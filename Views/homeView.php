a7c1c8b2234ab1586ac293a8bc9ab7bb071d350e
kvnbrnrd <kevos.directos@gmail.com>
2019-01-07 11:23:15 +0100

    modif chemin CSS et JS

--- Views/homeView.php
+++ Views/homeView.php
@@ -7,9 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="Assets/CSS/main.css" />
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <script src="main.js"></script>
+    <script src="Assets/JS/main.js"></script>
 </head>
 <body>
         <div class="container">
