baaf832d635a04d3db297864b965e78a9f0f28e8
Jaune Loke <Jaune.Loke@gmail.com>
2019-01-07 10:39:56 +0100

    modification liens assets

--- Views/homeView.php
+++ Views/homeView.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>We Transfer</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="Assets/main.css" />
 </head>
 <body>
-    
 
-   <script src="main.js"></script> 
+
+   <script src="Assets/main.js"></script> 
 </body>
 </html>
