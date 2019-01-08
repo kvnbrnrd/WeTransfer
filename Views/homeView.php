07eac06e2c6089e1362e993b94b5e0f222489ade
CuriouSound <robin.nota68@gmail.com>
2019-01-08 11:42:17 +0100

    changement icone title

--- Views/homeView.php
+++ Views/homeView.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Upload fichier</title>
+    <title>R&M Transfer</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
     <link rel="stylesheet" type="text/css" media="screen" href="Assets/CSS/main.css" />
+    <link rel="shortcut icon" href="Assets/Images/icone.ico" />
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <script src="Assets/JS/main.js"></script>
 </head>
